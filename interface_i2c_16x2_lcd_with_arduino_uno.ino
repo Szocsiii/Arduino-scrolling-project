aa64d30a2d3b157904dfb853be5716bdea17ee25
Szocsiii <90110339+Szocsiii@users.noreply.github.com>
2022-06-13 22:21:22 +0200

    interface_i2c_16x2_lcd_with_arduino_uno

---
+++ interface_i2c_16x2_lcd_with_arduino_uno.ino
@@ -0,0 +1,47 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+int sensorValue = 0;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+
+	lcd.begin();
+
+
+	lcd.backlight();
+	lcd.clear();
+	lcd.setCursor(4,0);
+	lcd.print("Hackster");
+}
+
+void loop()
+{
+  lcd.setCursor(0,0);
+lcd.print("  DIY Inventor");
+    
+
+  sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+
+lcd.setCursor(0,1);
+lcd.print(sensorValue);
+lcd.setCursor(4,1);
+lcd.print("Sensor Value");
+
+  for (int positionCounter = 0; positionCounter < analogRead(A0); positionCounter++) {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    // wait a bit:
+    delay(150);
+  }
+  for (int positionCounter = 0; positionCounter < analogRead(A0); positionCounter--) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(150);
+  }
+
+  delay(500);
+  lcd.clear();
+}
